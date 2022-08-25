068fe5d9d2fbdca9478df54637699205d9d7c890
j4 <hassan.shapp@gmail.com>
2022-08-25 23:54:47 +0000

    48q j4

--- ldpc_48q_odd_j4.py
+++ ldpc_48q_odd_j4.py
@@ -118,7 +118,7 @@ for p_r in p_r_list:
             succ_prob_word_X /= (Nrep)
 
             toc = time.time()
-            print("finished L = %d, r=%d in %.1f secs" % (Nl,i_rep,toc-tic))
+            print("finished p_r= %.2f, L = %d, r=%d in %.1f secs" % (p_r,Nl,i_rep,toc-tic))
 
             if bdy:
                 fname = "data_48q/" + "odd_p_%.2f_Nl_%d_i_%d.npz" % (p_r,Nl,i_rep)
